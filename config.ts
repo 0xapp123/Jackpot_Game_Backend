16ca6af5e39155b6302d38070ac4522148d179d1
0xed123 <apollo1030109@gmail.com>
2023-03-21 19:55:23 +0900

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,2 +1,10 @@
 export const FIRST_COOLDOWN = 35000;
 export const NEXT_COOLDOWN = 6000;
+
+let isProcessing: boolean = false;
+let pendingCount: number = 0;
+
+export const getProcessingStatus = () => isProcessing;
+export const setProcessingStatus = (s: boolean) => isProcessing = s;
+export const getPendingCount = () => pendingCount;
+export const setPendingCount = (cnt: number) => pendingCount = cnt;
